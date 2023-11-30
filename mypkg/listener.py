32db3ddeb1e9b9bdbc3c4f6bcd8ef0788a38baac
Maxi Kiyosawa <s22C1040UT@s.chibakoudai.jp>
2023-11-30 12:35:01 +0900

    organized publisher functions in class

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,32 +1,29 @@
 #!/usr/bin/env python3
 import rclpy
 from rclpy.node import Node
-from person_msgs.srv import Query
+from std_msgs.msg import Int16
+
+class Listener(Node):
+    def __init__(self):
+        super().__init__('listener')
+        self.subscription = self.create_subscription(
+            Int16, 
+            'countup',
+            self.listener_callback,
+            10
+        )
+
+    def listener_callback(self, msg):
+        if msg.data == 10:
+            self.get_logger().info('Listen: 10')
+        else:
+            self.get_logger().info('Received: "{}"'.format(msg.data))
 
 def main():
     rclpy.init()
-    node = Node("listener")
-    client = node.create_client(Query, 'query')  # サービスクライアントの作成
-    while not client.wait_for_service(timeout_sec=1.0):  # サービスの待機
-        node.get_logger().info('待機中')
-
-    req = Query.Request()
-    req.name = "Maxi Kiyosawa"
-    future = client.call_async(req)  # 非同期でサービスを呼び出し
-
-    while rclpy.ok():
-        rclpy.spin_once(node)  # サービスの処理待ち
-        if future.done():  # サービスの処理が完了したか確認
-            try:
-                response = future.result()  # 結果を受け取り
-            except Exception as e:
-                node.get_logger().info('呼び出し失敗: {}'.format(e))
-            else:
-                node.get_logger().info("age: {}".format(response.age))
-            break
-
-    node.destroy_node()  # ノードの後始末
-    rclpy.shutdown()     # ROS 2 シャットダウン
+    listener = Listener()
+    rclpy.spin(listener)
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
