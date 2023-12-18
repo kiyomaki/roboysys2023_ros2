16fe190a8bf889373d2e739e19acb1188367f12a
Maxi Kiyosawa <s22C1040UT@s.chibakoudai.jp>
2023-12-18 23:32:08 +0900

    renamed triples

--- test/test.bash
+++ test/test.bash
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 dir=~
-[ "$1" != "" ] && dir="$1"
+[ "$1" != "" ] && dir="$1"   # 引数があったら、そちらをホームに変える。
 
 cd $dir/ros2_ws
 colcon build
 source $dir/ros2_ws/install/setup.bash
 
+# 'ros2 launch' をバックグラウンドで実行
 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg.log &
 
+# ログファイルを監視し、'Triples!' メッセージが現れたら終了
 timeout=300  # 5分のタイムアウト
 elapsed=0
 
 while [ $elapsed -lt $timeout ]; do
-    if grep -q 'ゾロ目！' /tmp/mypkg.log; then
-        echo "Test passed: 'ゾロ目！' message was received."
+    if grep -q 'Triples! at count' /tmp/mypkg.log; then
+        echo "Test passed: 'Triples! at count' message was received."
         kill %1
         exit 0
     fi
