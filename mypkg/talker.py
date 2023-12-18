66a87476c3c759d3883c0667d115ba06a7b04834
Maxi Kiyosawa <s22C1040UT@s.chibakoudai.jp>
2023-12-18 12:26:16 +0900

    created triples talker program

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,25 +1,31 @@
 #!/usr/bin/env python3
+# SPDX-FileCopyrightText: 2023 Makishi Kiyosawa
+# SPDX-License-Identifier: BSD-3-Clause
+
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int16
+import random
 
 class Talker(Node):
     def __init__(self):
         super().__init__('talker')
         self.pub = self.create_publisher(Int16, "countup", 10)
-        self.n = 0
         self.create_timer(0.5, self.cb)
 
     def cb(self):
         msg = Int16()
-        msg.data = self.n
+        msg.data = random.randint(100, 999)  # 3桁のランダムな数字を生成
         self.pub.publish(msg)
-        self.n += 1
+
+        if msg.data % 111 == 0:  # ゾロ目の判定
+            self.get_logger().info('Triples!')
+            self.destroy_node()  # ゾロ目が出たらノードを終了
 
 def main():
     rclpy.init()
-    talker = Talker()  # Talker インスタンスの作成
-    rclpy.spin(talker)  # Talker インスタンスを spin に渡す
+    talker = Talker()
+    rclpy.spin(talker)
     rclpy.shutdown()
 
 if __name__ == '__main__':
