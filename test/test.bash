577d6edd6072d68ff5a62d3c41ade96dd66d24b4
Maxi Kiyosawa <s22C1040UT@s.chibakoudai.jp>
2023-12-19 08:04:57 +0900

    not while

--- test/test.bash
+++ test/test.bash
@@ -4,26 +4,23 @@
 dir=~
 [ "$1" != "" ] && dir="$1"
 cd $dir/ros2_ws
-sudo rosdep fix-permissions
-source source /opt/ros/humble/setup.bash
+source install/setup.bash
 
 # ノードの起動とログファイルの設定
 ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg_test.log &
 
-# テストのタイムアウト設定
-timeout=300
-elapsed=0
+# ノードが完全に起動するのを待つ
+sleep 5
 
-# ログファイルを監視し、特定のメッセージが現れるかをチェック
-while [ $elapsed -lt $timeout ]; do
-    if grep -q 'Triples! at count' /tmp/mypkg_test.log; then
-        echo "Test passed: 'Triples! at count' message was detected."
-        exit 0
-    fi
-    sleep 1
-    ((elapsed++))
-done
+# 1分間待機
+sleep 60
 
-echo "Test failed: Timed out after ${timeout} seconds."
-exit 1
+# ログファイルを確認し、特定のメッセージが現れるかをチェック
+if grep -q 'Triples! at count' /tmp/mypkg_test.log; then
+    echo "Test passed: 'Triples! at count' message was detected."
+    exit 0
+else
+    echo "Test failed: 'Triples! at count' message was not found."
+    exit 1
+fi
 
