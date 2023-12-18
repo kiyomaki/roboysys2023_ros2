0c9903e2b501accfbcf71977ff1b31f296fa5a5a
Maxi Kiyosawa <s22C1040UT@s.chibakoudai.jp>
2023-12-18 22:43:35 +0900

    added count program

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,32 +1,35 @@
 #!/usr/bin/env python3
-# SPDX-FileCopyrightText: 2023 Makishi Kiyosawa
+# Copyright 2023 Makishi Kiyosawa
 # SPDX-License-Identifier: BSD-3-Clause
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from std_msgs.msg import String
 
 class Listener(Node):
     def __init__(self):
         super().__init__('listener')
         self.subscription = self.create_subscription(
-            Int16,
+            String,
             'countup',
             self.listener_callback,
             10
         )
 
     def listener_callback(self, msg):
-        self.get_logger().info(f'Received: "{msg.data}"')
-        if msg.data % 111 == 0:  # ゾロ目の判定
-            self.get_logger().info('Triples!')
-            self.destroy_node()  # ゾロ目が出たらノードを終了
+        count, number = msg.data.split(':')
+        number = int(number)
+        self.get_logger().info(f'Received: "{number}" {count}回目')
+
+        if number % 111 == 0:
+            self.get_logger().info(f'ゾロ目！ {count}回目')
+            self.destroy_node()
+            rclpy.shutdown()
 
 def main():
     rclpy.init()
     listener = Listener()
     rclpy.spin(listener)
-    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
