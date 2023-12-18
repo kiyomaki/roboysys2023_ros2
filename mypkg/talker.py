c9415e8e117b9a5f36dfe797714f6ce408439964
Maxi Kiyosawa <s22C1040UT@s.chibakoudai.jp>
2023-12-18 22:43:14 +0900

    added count program

--- mypkg/talker.py
+++ mypkg/talker.py
@@ -1,32 +1,35 @@
 #!/usr/bin/env python3
-# SPDX-FileCopyrightText: 2023 Makishi Kiyosawa
+# Copyright 2023 Makishi Kiyosawa
 # SPDX-License-Identifier: BSD-3-Clause
 
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int16
+from std_msgs.msg import String
 import random
 
 class Talker(Node):
     def __init__(self):
         super().__init__('talker')
-        self.pub = self.create_publisher(Int16, "countup", 10)
+        self.pub = self.create_publisher(String, 'countup', 10)
+        self.count = 0
         self.create_timer(0.5, self.cb)
 
     def cb(self):
-        msg = Int16()
-        msg.data = random.randint(100, 999)  # 3桁のランダムな数字を生成
+        self.count += 1
+        random_number = random.randint(100, 999)
+        msg = String()
+        msg.data = f'{self.count}:{random_number}'
         self.pub.publish(msg)
 
-        if msg.data % 111 == 0:  # ゾロ目の判定
-            self.get_logger().info('Triples!')
-            self.destroy_node()  # ゾロ目が出たらノードを終了
+        if random_number % 111 == 0:
+            self.get_logger().info(f'ゾロ目！{self.count}回目')
+            self.destroy_node()
+            rclpy.shutdown()
 
 def main():
     rclpy.init()
     talker = Talker()
     rclpy.spin(talker)
-    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
