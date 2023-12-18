c56cae070232f28a3a8ec6f1d3df3808464afb3b
Maxi Kiyosawa <s22C1040UT@s.chibakoudai.jp>
2023-12-19 08:32:32 +0900

    use whle

--- test/test.bash
+++ test/test.bash
@@ -12,20 +12,20 @@ ros2 launch mypkg talk_listen.launch.py > /tmp/mypkg_test.log &
 # ノードが完全に起動するのを待つ
 sleep 5
 
-# 1分間待機
-sleep 60
+# テストのタイムアウト設定
+timeout=300
+elapsed=0
 
-# ログファイルの内容を表示（デバッグ用）
-echo "Log file contents:"
-cat /tmp/mypkg_test.log
-echo "End of log file contents"
+# ログファイルを監視し、特定のメッセージが現れるかをチェック
+while [ $elapsed -lt $timeout ]; do
+    if grep -q 'Triples! at count' /tmp/mypkg_test.log; then
+        echo "Test passed: 'Triples! at count' message was detected."
+        exit 0
+    fi
+    sleep 1
+    ((elapsed++))
+done
 
-# ログファイルを確認し、特定のメッセージが現れるかをチェック
-if grep -q 'Triples! at count' /tmp/mypkg_test.log; then
-    echo "Test passed: 'Triples! at count' message was detected."
-    exit 0
-else
-    echo "Test failed: 'Triples! at count' message was not found."
-    exit 1
-fi
+echo "Test failed: 'Triples! at count' message was not found after ${timeout} seconds."
+exit 1
 
