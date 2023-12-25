e018f969c8151715f5ebb7dcad1dcff4d5d30cca
Maxi Kiyosawa <s22C1040UT@s.chibakoudai.jp>
2023-12-25 17:53:25 +0900

    changed robot simulation program

--- mypkg/listener.py
+++ mypkg/listener.py
@@ -1,59 +1,58 @@
 #!/usr/bin/env python3
 # Copyright 2023 Makishi Kiyosawa
 # SPDX-License-Identifier: BSD-3-Clause
-
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+import tkinter as tk
+import threading
+
+class RobotSimulator:
+    def __init__(self, root):
+        self.root = root
+        self.canvas = tk.Canvas(root, width=400, height=400)
+        self.canvas.pack()
+        self.robot = self.canvas.create_rectangle(180, 180, 220, 220, fill="blue")
+
+    def move_robot(self, command):
+        if command == 'forward':
+            self.canvas.move(self.robot, 0, -10)
+        elif command == 'backward':
+            self.canvas.move(self.robot, 0, 10)
+        elif command == 'turn_left':
+            self.canvas.move(self.robot, -10, 0)
+        elif command == 'turn_right':
+            self.canvas.move(self.robot, 10, 0)
 
 class Listener(Node):
-    def __init__(self):
-        super().__init__('listener')
+    def __init__(self, simulator):
+        super().__init__('robot_listener')
         self.subscription = self.create_subscription(
-            String, 'countup', self.listener_callback, 10)
-        self.numbers = []
+            String, 'robot_commands', self.execute_command, 10)
+        self.simulator = simulator
 
-    def listener_callback(self, msg):
-        count, number = msg.data.split(':')
-        number = int(number)
-        self.numbers.append(number)
+    def execute_command(self, msg):
+        command = msg.data
+        self.simulator.move_robot(command)
+        # コマンド実行時のログメッセージを追加
+        self.get_logger().info(f'Robot is executing: {command}')
 
-        self.get_logger().info(f'Received: "{number}" at count {count}')
+def main(args=None):
+    rclpy.init(args=args)
+    root = tk.Tk()
+    simulator = RobotSimulator(root)
+    listener = Listener(simulator)
 
-        if number % 111 == 0:
-            self.calculate_and_display_statistics()
-            self.destroy_node()
-            rclpy.shutdown()
+    def spin():
+        rclpy.spin(listener)
+        listener.destroy_node()
+        rclpy.shutdown()
 
-    def calculate_and_display_statistics(self):
-        if self.numbers:
-            avg = sum(self.numbers) / len(self.numbers)
-            median = self.calculate_median(self.numbers)
-            total = sum(self.numbers)
-            max_value = max(self.numbers)
-            min_value = min(self.numbers)
+    spin_thread = threading.Thread(target=spin)
+    spin_thread.start()
 
-            self.get_logger().info(
-                f'Statistics - Count: {len(self.numbers)}, '
-                f'Average: {avg:.2f}, Median: {median}, '
-                f'Max: {max_value}, Min: {min_value}, '
-                f'Sum: {total}'
-            )
-
-    def calculate_median(self, data):
-        data = sorted(data)
-        n = len(data)
-        if n % 2 == 0:
-            mid1 = data[n // 2]
-            mid2 = data[n // 2 - 1]
-            return (mid1 + mid2) / 2
-        else:
-            return data[n // 2]
-
-def main():
-    rclpy.init()
-    listener = Listener()
-    rclpy.spin(listener)
+    root.mainloop()
+    spin_thread.join()
 
 if __name__ == '__main__':
     main()
